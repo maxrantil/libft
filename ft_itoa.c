a39a899f0407a15c25084295fecfadd69efff7fb
mrantil <mrantil@student.hive.fi>
2021-11-16 11:59:29 +0200

    ft_itoa try(not finnished)

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,47 +6,39 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 15:22:07 by mrantil           #+#    #+#             */
-/*   Updated: 2021/11/15 18:47:42 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/11/16 11:58:44 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_putstrnbr(int n)
+static int	n_length(int nbr)
 {
-	char	*s;
-	long	nbr;
-	int		i;
+	int		c;
 
-	i = 0;
-	nbr = (long)n;
-	s = (char *)ft_memalloc(sizeof(int));
-	if (!s)
-		return (NULL);
+	c = 0;
 	if (nbr < 0)
 	{
-		s[i++] = '-';
 		nbr *= -1;
 	}
 	if (nbr > 9)
 	{
-		ft_putstrnbr(nbr / 10);
-		ft_putstrnbr(nbr % 10);
+		n_length(nbr / 10);
+		n_length(nbr % 10);
 	}
 	else
 	{
-		s[i++] = nbr + 48;
+		c = c + 1;		
 	}
-	s[i] = '\0';
-	return (s);
+	return (c);
 }
 
 char    *ft_itoa(int n)
 {
-	char	*s;	
-	s = (char *)ft_memalloc(sizeof(int));
-	if (!s)
-		return (NULL);
-	s = ft_putstrnbr(n);
+	char	*s;
+	int		l;
+
+	l = n_length(n);
+	s = (char *)ft_memalloc(sizeof(char) * l +  1);
 	return (s);
 }
