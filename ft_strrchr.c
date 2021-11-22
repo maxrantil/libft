8fdf8e24a21ee8e54f8a62fb0a2fcba472c26a25
mrantil <mrantil@student.hive.fi>
2021-11-22 21:32:40 +0200

    final

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 17:16:41 by mrantil           #+#    #+#             */
-/*   Updated: 2021/11/10 19:26:32 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/11/22 21:29:50 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
-	int	j;
 
-	j = 0;
+	if (!s)
+		return (NULL);
 	i = ft_strlen(s) + 1;
 	while (*s)
 		s++;
-	while (i-- > j)
+	while (i-- > 0)
 	{
 		if ((char)c == *s)
 			return ((char *)s);
